790aa20d04e81d51ac27892c1f1ec16ddc893ac9
soroushyasini <soroush.yasini@mail.um.ac.ir>
2026-03-08 10:29:42 +0330

    Initial empty commit (clean start)

--- backend/app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .lookup import lookup_by_truck_number
-from .bulk_import import preview_bulk_import, confirm_bulk_import
-
-__all__ = ["lookup_by_truck_number", "preview_bulk_import", "confirm_bulk_import"]
--- frontend/src/api/lookup.js
+++
@@ -1,4 +0,0 @@
-import client from './client'
-
-export const lookupTruck = (truckNumber) =>
-  client.get(`/api/lookup/truck/${truckNumber}`).then((r) => r.data)
